f924fad83aee92ee15796b56201182f655fe663d
Desmond <63413811+desmondcoles1@users.noreply.github.com>
2025-10-23 15:42:48 -0500

    tried to translate more complicated code, still isn't working

--- leetcde_283_with_hax/proofs/lean/extraction/Leetcde_283_with_hax.lean
+++ leetcde_283_with_hax/proofs/lean/extraction/Leetcde_283_with_hax.lean
@@ -11,75 +11,13 @@ open Std.Tactic
 set_option mvcgen.warning false
 set_option linter.unusedVariables false
 
-def Leetcde_283_with_hax.move_zeroes
-  (nums : (Alloc.Vec.Vec i32 Alloc.Alloc.Global))
-  : Result (Alloc.Vec.Vec i32 Alloc.Alloc.Global)
-  := do
-  let n : usize ← (Alloc.Vec.Impl_1.len i32 Alloc.Alloc.Global nums);
-  let nums : (Alloc.Vec.Vec i32 Alloc.Alloc.Global) ←
-    (Core.Iter.Traits.Iterator.Iterator.fold
-      (← (Core.Iter.Traits.Collect.IntoIterator.into_iter
-        (← (Core.Iter.Traits.Iterator.Iterator.rev
-          (Core.Ops.Range.Range.mk
-            (start := (0 : usize)) (_end := (← (n -? (1 : usize)))))))))
-      nums
-      (fun nums i => (do
-        if (← (Rust_primitives.Hax.Machine_int.eq (← nums[i]_?) (0 : i32))) then
-          let nums : (Alloc.Vec.Vec i32 Alloc.Alloc.Global) ←
-            (Rust_primitives.Hax.Folds.fold_range
-              i
-              (← (n -? (1 : usize)))
-              (fun nums _ => (do (pure true) : Result Bool))
-              nums
-              (fun nums j => (do
-                (Rust_primitives.Hax.Monomorphized_update_at.update_at_usize
-                  nums
-                  j
-                  (← nums[(← (j +? (1 : usize)))]_?)) : Result
-                (Alloc.Vec.Vec i32 Alloc.Alloc.Global))));
-          let nums : (Alloc.Vec.Vec i32 Alloc.Alloc.Global) ←
-            (Rust_primitives.Hax.Monomorphized_update_at.update_at_usize
-              nums
-              (← (n -? (1 : usize)))
-              (0 : i32));
-          (pure nums)
-        else
-          (pure nums) : Result (Alloc.Vec.Vec i32 Alloc.Alloc.Global))));
-  (pure nums)
-
--- manual test
 def Leetcde_283_with_hax.main
   (_ : Rust_primitives.Hax.Tuple0)
   : Result Rust_primitives.Hax.Tuple0
   := do
-  let nums : (Alloc.Vec.Vec i32 Alloc.Alloc.Global) ←
-    (Alloc.Slice.Impl.to_vec i32
-      (← (Rust_primitives.unsize
-        #v[(0 : i32),
-             (0 : i32),
-             (0 : i32),
-             (1 : i32),
-             (0 : i32),
-             (0 : i32),
-             (0 : i32),
-             (0 : i32),
-             (2 : i32),
-             (0 : i32),
-             (1 : i32),
-             (1 : i32),
-             (1 : i32),
-             (0 : i32),
-             (2 : i32),
-             (20 : i32),
-             (1 : i32),
-             (0 : i32),
-             (0 : i32),
-             (0 : i32)])));
-  let nums : (Alloc.Vec.Vec i32 Alloc.Alloc.Global) ←
-    (Leetcde_283_with_hax.move_zeroes nums);
   (pure Rust_primitives.Hax.Tuple0.mk)
 
-def Leetcde_283_with_hax.move_zeroes_better
+def Leetcde_283_with_hax.move_zeroes
   (nums : (Alloc.Vec.Vec i32 Alloc.Alloc.Global))
   : Result (Alloc.Vec.Vec i32 Alloc.Alloc.Global)
   := do
@@ -116,3 +54,32 @@ def Leetcde_283_with_hax.move_zeroes_better
           i
           (0 : i32)) : Result (Alloc.Vec.Vec i32 Alloc.Alloc.Global))));
   (pure nums)
+
+def Leetcde_283_with_hax.move_zeroes_alt
+  (nums : (Alloc.Vec.Vec i32 Alloc.Alloc.Global))
+  : Result (Alloc.Vec.Vec i32 Alloc.Alloc.Global)
+  := do
+  let result : (Alloc.Vec.Vec i32 Alloc.Alloc.Global) ←
+    (Alloc.Vec.Impl.new i32 Rust_primitives.Hax.Tuple0.mk);
+  let zeros : usize := (0 : usize);
+  let ⟨result, zeros⟩ ←
+    (Core.Iter.Traits.Iterator.Iterator.fold
+      (← (Core.Iter.Traits.Collect.IntoIterator.into_iter nums))
+      (Rust_primitives.Hax.Tuple2.mk result zeros)
+      (fun ⟨result, zeros⟩ x => (do
+        if (← (Rust_primitives.Hax.Machine_int.ne x (0 : i32))) then
+          let result : (Alloc.Vec.Vec i32 Alloc.Alloc.Global) ←
+            (Alloc.Vec.Impl_1.push i32 Alloc.Alloc.Global result x);
+          (pure (Rust_primitives.Hax.Tuple2.mk result zeros))
+        else
+          let zeros : usize ← (zeros +? (1 : usize));
+          (pure (Rust_primitives.Hax.Tuple2.mk result zeros)) : Result
+        (Rust_primitives.Hax.Tuple2
+          (Alloc.Vec.Vec i32 Alloc.Alloc.Global)
+          usize))));
+  let result : (Alloc.Vec.Vec i32 Alloc.Alloc.Global) ←
+    (Core.Iter.Traits.Collect.Extend.extend
+      (Alloc.Vec.Vec i32 Alloc.Alloc.Global)
+      result
+      (← (Alloc.Vec.from_elem i32 (0 : i32) zeros)));
+  (pure result)
--- leetcde_283_with_hax/src/main.rs
+++ leetcde_283_with_hax/src/main.rs
@@ -1,30 +1,14 @@
 //Given an integer array nums, move all 0's to the end of it 
 //while maintaining the relative order of the non-zero elements.
 //Note that you must do this in-place without making a copy of the array.
-pub fn move_zeroes(nums: &mut Vec<i32>) {
-    let n = nums.len();
-    for i in (0..n-1).rev(){
-        if nums[i] == 0 {
-            for j in i..n-1 {
-                nums[j]= nums[j+1]
-            }
-            nums[n-1] = 0;
-        } 
-    }
-}
-
-///manual test
-
 fn main() {
-    let mut nums = [0,0,0,1,0,0,0,0,2,0,1,1,1,0,2,20,1,0,0,0].to_vec();
-    move_zeroes(&mut nums);
-    //println!("{:?}", move_zeroes(&mut nums))
+
 }
 
 
-//here is a faster solution
+//here is a solution that doesn't translate well
 
-pub fn move_zeroes_better(nums: &mut Vec<i32>) {
+pub fn move_zeroes(nums: &mut Vec<i32>) {
     let mut insert_pos = 0;
 
     // Move all non-zero elements forward
@@ -42,3 +26,20 @@ pub fn move_zeroes_better(nums: &mut Vec<i32>) {
 }
 
 
+// here's another possible solution
+
+pub fn move_zeroes_alt(nums: Vec<i32>) -> Vec<i32> {
+    let mut result = vec![];
+    let mut zeros = 0;
+
+    for &x in &nums {
+        if x != 0 {
+            result.push(x);
+        } else {
+            zeros += 1;
+        }
+    }
+
+    result.extend(vec![0; zeros]);
+    result
+}
